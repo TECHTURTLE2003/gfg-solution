c0884884f92a28514935dd2b2640f9b05f049741
SOUVIK JANA <129412320+TECHTURTLE2003@users.noreply.github.com>
2025-03-16 12:01:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            System.out.println(new Solution().minJumps(arr));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    static int minJumps(int[] arr) {
+        // code here
+         int max=0,jump=0,current=0;
+        for(int i=0;i<arr.length;i++){
+            max=Math.max(max,i+arr[i]);
+            if(current==i){
+                if(i!=arr.length-1){
+                    jump++;
+                }
+                current=max;
+            }else{
+                continue;
+            }
+        }
+        if(current<arr.length-1){
+                return -1;
+        }
+        return jump;
+    }
+}
